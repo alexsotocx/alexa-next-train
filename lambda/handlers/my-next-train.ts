2ccae6e212fc54527285d95ed41c4bea5461bc8c
Alexander Soto <alexsotocx@gmail.com>
2024-04-01 16:01:41 +0200

    Integrate services into next train intent

--- lambda/handlers/my-next-train.ts
+++ lambda/handlers/my-next-train.ts
@@ -1,5 +1,13 @@
 import { HandlerInput, RequestHandler } from "ask-sdk-core";
 import { Response } from "ask-sdk-model";
+import { MVGService } from "../services/mvg.api";
+import { HttpService } from "../services/http.service";
+import axios from "axios";
+import { MVGDepartureAdapter } from "../services/departure.handler";
+import { createAlexaResponseDeparture } from "../views/next-train.view";
+import { lastValueFrom, map } from "rxjs";
+
+const aubingStationId = 'de:09162:1730';
 
 export const MyNextTrainHandler: RequestHandler = {
     canHandle(handlerInput: HandlerInput): boolean {
@@ -7,12 +15,22 @@ export const MyNextTrainHandler: RequestHandler = {
         return request.type === 'IntentRequest'
             && request.intent.name === 'MyNextTrain';
     },
-    handle(handlerInput: HandlerInput): Response {
-        const speechText = 'Tu siguiente tren es en 10 min';
+    handle(handlerInput: HandlerInput): Promise<Response> {
+        const mvgService = new MVGService(new HttpService(axios.create({
+            baseURL: 'https://www.mvg.de/api/fib/v2/',
+        })));
 
-        return handlerInput.responseBuilder
-            .speak(speechText)
-            .withSimpleCard('Tu siguiente tren es en 10 min', speechText)
-            .getResponse();
+        const departureHandler = new MVGDepartureAdapter(mvgService);
+        const departuresObservable = departureHandler.getDepartures(aubingStationId, ['Flughafen München', 'Ostbahnhof', 'Hauptbahnhof']);
+
+        return lastValueFrom(departuresObservable.pipe(
+            map(departures => createAlexaResponseDeparture(departures)),
+            map((speechText) => {
+                return handlerInput.responseBuilder
+                    .speak(speechText)
+                    .withSimpleCard('Siguiente tren', speechText)
+                    .getResponse();
+            })
+        ));
     },
 };
